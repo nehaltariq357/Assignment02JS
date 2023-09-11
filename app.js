bcd1c2a4550c92f1ab6e2d28d498a5e7ff2a336e
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2023-09-11 00:59:32 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,295 @@
+// CHAPTER #06 
+
+
+// QUESTION #01
+
+// x++;
+
+
+// QUESTION #02
+
+// x--
+
+
+// QUESTION #03
+
+// x becomes 51 and y holds the value of x which is 50
+
+
+
+// QUESTION #04
+
+
+// after  decrement z will be 49, and y will be 49 after the decrement.
+
+
+// QUESTION #05
+
+
+// var newNum = num--;
+
+
+// QUESTION #06
+
+// var orignalValue = thisVariable++;
+
+
+// QUESTION #07
+
+
+
+var numberValue = 8;
+numberValue++;
+alert("new value");
+
+// CHAPTER #06 COMPLETED
+
+
+// CHAPTER #07 
+
+
+// QUESTION #01
+
+var calculatedNum = 2 + (2 * 6);
+
+var calculatedNum = 14;
+
+// QUESTION #02
+
+var caluculatedNum = (2 + 2) * 6;
+var calculatedNum = 24
+
+// QUESTION #03
+
+
+// 3. var calculatedNum = (2 + 2) * (4 + 2);
+// What is the value of calculatedNum?
+// 24
+
+// QUESTION #04
+
+// 4. var calculatedNum = ((2 + 2) * 4) + 2;
+
+// What is the value of calculatedNum?
+// 18
+
+
+
+// QUESTION #05
+
+
+var cost = (2+2)*(4+10);
+
+
+// QUESTION #06
+
+var units = 2 + (2 * 4) + 10;
+
+// QUESTION #07
+
+
+var pressure = (4 / 2) * 4;
+
+
+
+// CHAPTER #07 COMPLETED
+
+
+
+// CHAPTER #08
+
+
+// QUESTION #01
+
+var numValue ="22";
+
+
+// QUESTION #02
+
+
+var messageValue = "HEllo,Dolly";
+alert("HelloDolly");
+
+// QUESTION #03
+
+// 333 is display in alert box;
+
+
+
+// QUESTION #04
+
+
+alert("Pakistan" + "Zindabad");
+
+
+// QUESTION #05
+
+var y ="example" + 123;
+
+
+// QUESTION #06
+
+
+var string1 ="HEllo";
+var string2 ="World";
+var result = string1 + string2;
+
+// CHAPTER #08 COMPLED 
+
+
+
+// CHAPTER #09 
+
+
+// QUESTION #01
+
+var firstName = prompt("Enter FIrst Name");
+
+
+// QUESTION #02
+
+var country = prompt("country ?" , "china");
+
+
+// QUESTION #03
+
+var yourName = prompt("Enter your Name");
+
+
+// QUESTION #04
+
+
+var input = prompt("Please Enter Your Email" , "example123@gmail.com");
+
+
+
+// QUESTION #05
+
+var question1 = "whats your favaurite color ";
+var question2 = "black"
+var question3 = prompt(question1,question2)
+
+
+
+// QUESTION #06
+
+
+var question77 = "what is your favourite game";
+var question88 ="football";
+var question99 =prompt(question77,question88);
+alert("choose :" + question99);
+
+// CHAPTER #09 COPLETED
+
+
+// CHAPTER #10
+
+
+// QUESTION #01
+
+var city = "karachi";
+if(city = "karachi"){
+    console.log("The City Of");
+}
+
+
+
+// QUESTION #02
+
+
+
+
+if (x === y) {
+
+    var z = prompt("Enter the value of z?");
+  }
+
+  
+
+
+
+
+// QUESTION #03
+
+var zipCode = "10010";
+
+if (zipCode === "10010") {
+
+    alert("Karachi");
+
+} else {
+
+    alert("Lahore");
+}
+
+
+
+// QUESTION #04
+
+
+
+var x = 1;
+
+if (x === 1) {
+    x = 6;
+}
+
+
+
+// CHAPTER #10 COPLETED 
+
+
+
+// CHAPTER #11 
+
+
+
+// QUESTION #01
+
+if (variable44 !== variable99) {
+ 
+}
+
+
+
+
+// QUESTION #02
+
+
+if (variable5 >= variable9) {
+ 
+}
+
+
+
+// QUESTION #03
+
+
+if (variable1 !== 5) {
+  variable1 = 10; 
+}
+
+
+
+
+// QUESTION #04
+
+
+if (number1 !== 7) {
+  alert("Congratulations!");
+}
+
+
+
+
+// QUESTION #05
+
+
+
+
+var enteredName = prompt("Please enter your first name:");
+var otherName = "Nehal";
+
+if (enteredName !== otherName) {
+  alert("No match");
+}
